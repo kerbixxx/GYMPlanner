a4ee9dac67798f386113df98c2c56b2baf06c0fc
kerbix <kerbixcs@mail.ru>
2024-04-11 15:41:14 +0700

    removed trash

--- Project/DTOs/Authorization/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GymPlanner.WebUI.DTOs.Authorization
-{
-    public class LoginDTO
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
