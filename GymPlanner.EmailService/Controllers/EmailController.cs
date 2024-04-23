dff4a6f603f216f2f7ee3881d7ad916c3170bc28
kerbix <kerbixcs@mail.ru>
2024-04-23 14:41:40 +0700

    EmailService is working

---
+++ GymPlanner.EmailService/Controllers/EmailController.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace EmailService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Email()
+        {
+            var message = new MimeMessage();
+            //от кого отправляем и заголовок
+            message.From.Add(new MailboxAddress("Test Project",HiddenData.SenderEmailLogin));
+            //кому отправляем
+            message.To.Add(new MailboxAddress("Tom", "test@test.ru"));
+            //тема письма
+            message.Subject = "Тестовое письмо для приятеля!";
+            //тело письма
+            message.Body = new TextPart("plain")
+            {
+                Text = "Доброго времени суток. Если вы получили это пис"
+            };
+            using (var client = new SmtpClient())
+            {
+                //Указываем smtp сервер почты и порт
+                client.Connect("smtp.gmail.com", 587, false);
+                //Указываем свой Email адрес и пароль приложения
+                client.Authenticate(HiddenData.SenderEmailLogin, HiddenData.SenderEmailPassword);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return Ok();
+        }
+    }
+}
