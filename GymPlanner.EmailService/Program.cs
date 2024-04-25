b1545d9aed1994db1149f69d955680a2a098cf75
kerbix <kerbixcs@mail.ru>
2024-04-25 15:05:36 +0700

    HangFire is on a 5 minute schedule

--- GymPlanner.EmailService/Program.cs
+++ GymPlanner.EmailService/Program.cs
@@ -2,13 +2,14 @@ using EmailService.Services;
 using Hangfire;
 using Microsoft.Extensions.Configuration;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHangfire(s => s.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangFire")));
 
 builder.Services.AddHangfireServer();
-builder.Services.AddSingleton<RabbitMqListener>();
+builder.Services.AddTransient<RabbitMqListener>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,8 +23,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHangfireServer();
 app.UseHangfireDashboard("/hangfire");
 
 app.UseHttpsRedirection();
@@ -34,6 +33,6 @@ app.MapControllers();
 
 var rabbitMqListener = app.Services.GetRequiredService<RabbitMqListener>();
 
-RecurringJob.AddOrUpdate(() => rabbitMqListener.Consume(), Cron.MinuteInterval(1));
+RecurringJob.AddOrUpdate("emailJob", () => rabbitMqListener.Consume(), "*/5 * * * *"); // Запуск каждые 5 минут
 
 app.Run();
--- GymPlanner.EmailService/Services/RabbitMqListener.cs
+++ GymPlanner.EmailService/Services/RabbitMqListener.cs
@@ -1,4 +1,5 @@
 ﻿using EmailService.DTOs;
+using Hangfire.Common;
 using MailKit.Net.Smtp;
 using MimeKit;
 using Newtonsoft.Json;
@@ -10,29 +11,35 @@ namespace EmailService.Services
 {
     public class RabbitMqListener
     {
-        private readonly IConnection _connection;
-        private readonly IModel _channel;
-        public RabbitMqListener()
-        {
-            var factory = new ConnectionFactory { HostName = "localhost", Port = 5673 };
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
-            _channel.QueueDeclare(queue: "EmailQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
-        }
+        private IConnection _connection;
+        private IModel _channel;
 
         public void Consume()
         {
-            MessagePlanEditConsumer message = new();
-            var consumer = new EventingBasicConsumer(_channel);
-            consumer.Received += (model, ea) =>
+            try
             {
-                var body = ea.Body.ToArray();
-                var json = Encoding.UTF8.GetString(body);
-                message = JsonConvert.DeserializeObject<MessagePlanEditConsumer>(json);
-                SendEmail(message.SubscriberEmail, message.PlanName);
-            };
+                var factory = new ConnectionFactory { HostName = "localhost", Port = 5673 };
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                _channel.QueueDeclare(queue: "EmailQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                MessagePlanEditConsumer message = new();
+                var consumer = new EventingBasicConsumer(_channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var json = Encoding.UTF8.GetString(body);
+                    message = JsonConvert.DeserializeObject<MessagePlanEditConsumer>(json);
+                    SendEmail(message.SubscriberEmail, message.PlanName);
+                };
 
-            _channel.BasicConsume(queue: "EmailQueue", autoAck: true, consumer: consumer);
+                _channel.BasicConsume(queue: "EmailQueue", autoAck: true, consumer: consumer);
+            }
+            finally
+            {
+                // Выключение
+                _channel?.Close();
+                _connection?.Close();
+            }
         }
         private void SendEmail(string email, string planName)
         {
